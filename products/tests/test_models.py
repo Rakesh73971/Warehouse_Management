2d33ed1a0df8442599425b57439f9656e33450fc
Rakesh N <nraki522@gmail.com>
2026-02-28 11:52:37 +0530

    tested using pytest fixture

--- products/tests/test_models.py
+++ products/tests/test_models.py
@@ -1,145 +1,22 @@
-import pytest
-from products.models import Category,Product,Inventory,StockMovement
-from warehouse.models import StorageType,Bin,Rack,Zone,Warehouse
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
+def test_category(category):
+    assert category.name == 'fruits'
 
-@pytest.mark.django_db
-def test_category():
-    category = Category.objects.create(
-        name='shoes'
-    )
 
-    assert category.name == 'shoes'
+def test_product_model(product):
 
-@pytest.mark.django_db
-def test_product_model():
-    category = Category.objects.create(
-        name = 'clothes'
-    )
-    storage = StorageType.objects.create(
-        name='Cold Storage',
-        temperature_range='10 to 25c'
-    )
-    product = Product.objects.create(
-        name='shirts',
-        sku='',
-        storage_type = storage,
-        category = category
-    )
-
-    assert product.category.name == 'clothes'
-    assert product.name == 'shirts'
+    assert product.category.name == 'fruits'
+    assert product.name == 'Banana'
     assert product.storage_type.name == 'Cold Storage'
 
-@pytest.mark.django_db
-def test_inventory_model():
-    category = Category.objects.create(
-        name='clothes'
-    )
-    storage = StorageType.objects.create(
-        name='Cold Storage',
-        temperature_range='10 to 25'
-    )
-    product  = Product.objects.create(
-        name='shirts',
-        sku='',
-        storage_type=storage,
-        category=category
-    )
+def test_inventory_model(inventory):
 
-    user = User.objects.create(
-        email='manager@gmail.com',
-        full_name='Test Manager',
-        password='password123',
-        role='MANAGER'
-    )
-    warehouse = Warehouse.objects.create(
-        name='Main Warehouse',
-        location='Hyderabad',
-        manager=user
-    )
-    zone = Zone.objects.create(
-        warehouse = warehouse,
-        name = 'Zone X',
-        description = ' '
-    )
-
-    rack = Rack.objects.create(
-        zone = zone,
-        rack_code = 'R001',
-        max_capacity = 100
-    )
-
-    bin = Bin.objects.create(
-        rack = rack,
-        bin_code = 'B001',
-        max_capacity = 100,
-        current_capacity = 50
-    )
-    inventory = Inventory.objects.create(
-        product = product,
-        bin = bin,
-        quantity = 10
-    )
-
-    assert inventory.product.name == 'shirts'
+    assert inventory.product.name == 'Banana'
     assert inventory.bin.rack.zone.warehouse.name == 'Main Warehouse'
 
-@pytest.mark.django_db
-def test_stockmovement_model():
-    category = Category.objects.create(
-        name='clothes'
-    )
-    storage = StorageType.objects.create(
-        name='Cold Storage',
-        temperature_range='10 to 25'
-    )
-    product  = Product.objects.create(
-        name='shirts',
-        sku='',
-        storage_type=storage,
-        category=category
-    )
 
-    user = User.objects.create(
-        email='manager@gmail.com',
-        full_name='Test Manager',
-        password='password123',
-        role='MANAGER'
-    )
-    warehouse = Warehouse.objects.create(
-        name='Main Warehouse',
-        location='Hyderabad',
-        manager=user
-    )
-    zone = Zone.objects.create(
-        warehouse = warehouse,
-        name = 'Zone X',
-        description = ' '
-    )
-
-    rack = Rack.objects.create(
-        zone = zone,
-        rack_code = 'R001',
-        max_capacity = 100
-    )
-
-    bin = Bin.objects.create(
-        rack = rack,
-        bin_code = 'B001',
-        max_capacity = 100,
-        current_capacity = 50
-    )
-    stockmovement = StockMovement.objects.create(
-        product = product,
-        bin = bin,
-        quantity = 3,
-        movement_type = 'Inbound'
-    )
-
-    assert stockmovement.product.name == 'shirts'
-    assert stockmovement.quantity == 3
+def test_stockmovement_model(stockmovement):
+    assert stockmovement.product.name == 'Banana'
+    assert stockmovement.quantity == 5
     assert stockmovement.bin.rack.zone.warehouse.name == 'Main Warehouse'
     assert stockmovement.bin.rack.zone.warehouse.manager.full_name == 'Test Manager'
--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -1,164 +1,68 @@
-import pytest
+
 from products.models import Category,Product,Inventory,StockMovement
-from django.contrib.auth import get_user_model
-from rest_framework.test import APIClient
-from warehouse.models import StorageType,Warehouse,Zone,Rack,Bin
-
-
-User = get_user_model()
-
-@pytest.mark.django_db
-class TestProductAPI:
-
-    def setup_method(self):
-        self.client = APIClient()
-        self.user = User.objects.create(
-            email='manager@gmail.com',
-            full_name='Test Manager',
-            password='password123',
-            role='MANAGER'
-        )
-        self.client.force_authenticate(user=self.user)
 
 
     # --------------
     # CATEGORY TEST
     # --------------
-    def test_create_category(self):
-        response = self.client.post(
-            '/api/product/categories/',
-            {
-                'name':'clothes'
-            },
-            format='json'
-        )
-        assert response.status_code == 201
-        assert Category.objects.count() == 1
+def test_create_category(auth_client):
+    response = auth_client.post(
+        '/api/product/categories/',
+        {
+            'name':'clothes'
+        },
+        format='json'
+    )
+    assert response.status_code == 201
+    assert Category.objects.count() == 1
 
-    # -------------
-    # PRODUCT TEST
-    # -------------
-    def test_create_product(self):
-        category = Category.objects.create(
-            name='fruits'
-        )
-        storage = StorageType.objects.create(
-            name = 'Cold Storage',
-            temperature_range = '10 to 25c'
-        )
-        response = self.client.post(
-            '/api/product/products/',
-            {
-                'name':'banana',
-                'sku':'BAN001',
-                'storage_type':storage.id,
-                'category':category.id
-            },
-            format='json'
-        )
+# -------------
+# PRODUCT TEST
+# -------------
+def test_create_product(auth_client,category,storage_type):
+    response = auth_client.post(
+        '/api/product/products/',
+        {
+            'name':'banana',
+            'sku':'BAN001',
+            'storage_type':storage_type.id,
+            'category':category.id
+        },
+        format='json'
+    )
 
-        assert response.status_code == 201
-        assert Product.objects.count() == 1
+    assert response.status_code == 201
+    assert Product.objects.count() == 1
 
-    # ---------------
-    # INVENTORY TEST
-    # ---------------
-    def test_create_inventory(self):
-        category = Category.objects.create(
-            name='fruits'
-        )
-        storage = StorageType.objects.create(
-            name='Cold Storage',
-            temperature_range='10 to 25c'
-        )
-        product = Product.objects.create(
-            name='banana',
-            sku='BAN001',
-            storage_type=storage,
-            category=category
-        )
-        warehouse = Warehouse.objects.create(
-            name='Main Warehouse',
-            location='Hyderabad',
-            manager=self.user
-        )
-        zone = Zone.objects.create(
-            warehouse = warehouse,
-            name = 'Zone X',
-            description = ' '
-        )
-
-        rack = Rack.objects.create(
-            zone = zone,
-            rack_code = 'R001',
-            max_capacity = 100
-        )
-
-        bin = Bin.objects.create(
-            rack = rack,
-            bin_code = 'B001',
-            max_capacity = 100,
-            current_capacity = 50
-        )
-        response = self.client.post(
-            '/api/product/inventories/',
-            {
-                'product':product.id,
-                'bin':bin.id,
-                'quantity':5
-            },
-            format='json'
-        )
-
-        assert response.status_code == 201
-        assert Inventory.objects.count() == 1
+# ---------------
+# INVENTORY TEST
+# ---------------
+def test_create_inventory(auth_client,product,bin_instance):
     
-    def test_create_stockmovement(self):
-        category = Category.objects.create(
-            name='fruits'
-        )
-        storage = StorageType.objects.create(
-            name='Cold Storage',
-            temperature_range='10 to 25c'
-        )
-        product = Product.objects.create(
-            name='banana',
-            sku='BAN002',
-            storage_type=storage,
-            category=category
-        )
-        warehouse = Warehouse.objects.create(
-            name='Main Warehouse',
-            location='Hyderabad',
-            manager=self.user
-        )
-        zone = Zone.objects.create(
-            warehouse = warehouse,
-            name = 'Zone X',
-            description = ' '
-        )
+    response = auth_client.post(
+        '/api/product/inventories/',
+        {
+            'product':product.id,
+            'bin':bin_instance.id,
+            'quantity':5
+        },
+        format='json'
+    )
 
-        rack = Rack.objects.create(
-            zone = zone,
-            rack_code = 'R001',
-            max_capacity = 100
-        )
+    assert response.status_code == 201
+    assert Inventory.objects.count() == 1
 
-        bin = Bin.objects.create(
-            rack = rack,
-            bin_code = 'B001',
-            max_capacity = 100,
-            current_capacity = 50
-        )
-        response = self.client.post(
-            '/api/product/stockmovements/',
-            {
-                'product':product.id,
-                'bin':bin.id,
-                'quantity':5,
-                'movement_type':'INBOUND'
-            },
-            format='json'
-        )
-        assert response.status_code == 201
-        assert StockMovement.objects.count() == 1
+def test_create_stockmovement(auth_client,product,bin_instance):
+    
+    response = auth_client.post(
+        '/api/product/stockmovements/',
+        {
+            'product':product.id,
+            'bin':bin_instance.id,
+            'quantity':5,
+            'movement_type':'INBOUND'
+        },
+        format='json'
+    )
+    assert response.status_code == 201
+    assert StockMovement.objects.count() == 1
