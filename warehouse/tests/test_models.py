11cc362b5babdab0b41635320c98cb3f5d9c3559
Rakesh N <nraki522@gmail.com>
2026-02-28 11:11:55 +0530

    used fixture testing

--- warehouse/tests/test_models.py
+++ warehouse/tests/test_models.py
@@ -1,130 +1,27 @@
-import pytest
-from django.contrib.auth import get_user_model
-from warehouse.models import Warehouse,StorageType,Zone,Rack,Bin
-
-User = get_user_model()
-
-@pytest.mark.django_db
-def test_create_warehouse():
-    user = User.objects.create(
-        email='manager@gmail.com',
-        full_name="Test Manager",
-        password="password123",
-        role="MANAGER"
-    )
-
-    warehouse = Warehouse.objects.create(
-        name = 'Central Warehouse',
-        location = 'Hyderabad',
-        manager=user
-        
-    )
-
+def test_create_warehouse(warehouse):
     assert warehouse.manager.email == 'manager@gmail.com'
-    assert str(warehouse) == 'Central Warehouse'
+    assert str(warehouse) == 'Main Warehouse'
 
 
-@pytest.mark.django_db
-def test_storagetype():
-    storagetype = StorageType.objects.create(
-        name = 'SV storages',
-        temperature_range = '-100'
-    )
+def test_storage_type(storage_type):
+    assert storage_type.name == "Dry Storage"
+    assert storage_type.temperature_range == "10 to 25°C"
 
-    assert storagetype.name == 'SV storages'
-    assert storagetype.temperature_range == '-100'
-
-
-@pytest.mark.django_db
-def test_zone():
-    user = User.objects.create(
-        email='manager@gmail.com',
-        full_name='Test Manager',
-        password='password123',
-        role='MANAGER'
-    )
-    warehouse = Warehouse.objects.create(
-        name = 'Main Warehouse',
-        location = 'Hyderabad',
-        manager = user
-    )
-    storage = StorageType.objects.create(
-        name = 'Dry storage',
-        temperature_range = "10 to 25°C"
-    )
-    
-    zone = Zone.objects.create(
-        warehouse = warehouse,
-        name = 'Zone A',
-        description = " ",
-        storage_type = storage
-    )
+def test_zone_creation(zone):
     assert zone.warehouse.name == 'Main Warehouse'
-    assert zone.storage_type.name == 'Dry storage'
-    assert str(zone) == "Main Warehouse - Zone A"
+    assert zone.storage_type.name == 'Dry Storage'
+    assert str(zone) == 'Main Warehouse - Zone A'
 
-
-@pytest.mark.django_db
-def test_rack_creation():
-    user = User.objects.create(
-        email='manager@gmail.com',
-        full_name='Test Manager',
-        password='password123',
-        role='MANAGER'
-    )
-    warehouse = Warehouse.objects.create(
-        name = 'Main Warehouse',
-        location = 'Hyderabad',
-        manager = user
-    )
-    zone = Zone.objects.create(
-        warehouse = warehouse,
-        name = 'Zone 1'
-    )
-    rack = Rack.objects.create(
-        zone = zone,
-        rack_code = 'R001',
-        max_capacity = 100
-    )
-
+def test_rack_creation(rack):
     assert rack.zone.warehouse.name == 'Main Warehouse'
-    assert str(rack) == "Rack R001"
+    assert str(rack) == 'Rack R001'
 
-@pytest.mark.django_db
-def test_bin_capacity_and_availability():
-    user = User.objects.create(
-        email='manager@gmail.com',
-        full_name='Test Manager',
-        password='password123',
-        role='MANAGER'
-    )
-    warehouse = Warehouse.objects.create(
-        name='Main Warehouse',
-        location='Hyderabad',
-        manager=user
-    )
-    zone = Zone.objects.create(
-        warehouse = warehouse,
-        name = 'Zone X',
-        description = ' '
-    )
+def test_bin_creation(bin_instance):
+    assert bin_instance.is_available is True
 
-    rack = Rack.objects.create(
-        zone = zone,
-        rack_code = 'R001',
-        max_capacity = 100
-    )
+    bin_instance.current_capacity = 100
+    bin_instance.save()
+    bin_instance.refresh_from_db()
 
-    bin = Bin.objects.create(
-        rack = rack,
-        bin_code = 'B001',
-        max_capacity = 100,
-        current_capacity = 50
-    )
-    assert bin.is_available is True
+    assert bin_instance.is_available is False
 
-    bin.current_capacity = 100
-    bin.save()
-
-    bin.refresh_from_db()
-    assert bin.is_available is False
--- warehouse/tests/test_views.py
+++ warehouse/tests/test_views.py
@@ -1,158 +1,104 @@
-import pytest
-from django.contrib.auth import get_user_model
-from rest_framework.test import APIClient
-from warehouse.models import Warehouse,StorageType,Zone,Rack,Bin
+from warehouse.models import Warehouse,Rack,Bin,StorageType,Zone
 
-User = get_user_model()
+# -----------------
+# WAREHOUSE TEST
+# -----------------
+def test_create_warehouse(auth_client,manager_user):
+    response = auth_client.post(
+        '/api/warehouse/warehouses/',
+        {
+            "name":"Main Warehouse",
+            "location":"Hyderabad",
+            "manager":manager_user.id
+        },
+        format='json'
+    )
 
-@pytest.mark.django_db
-class TestWarehouseSystemAPI:
+    assert response.status_code == 201
+    assert Warehouse.objects.count() == 1
 
-    def setup_method(self):
-        self.client = APIClient()
-        self.user = User.objects.create_user(
-            email="manager@test.com",
-            full_name="Manager",
-            password="password123",
-            role="MANAGER"
-        )
-        self.client.force_authenticate(user=self.user)
+def test_filter_warehouse_by_location(auth_client,manager_user):
+    Warehouse.objects.create(name='W1',location='Delhi',manager=manager_user)
+    Warehouse.objects.create(name='W2',location='Hyderabad',manager=manager_user)
 
-    #-----------------
-    #WAREHOUSE TESTS
-    #-----------------
-    def test_create_warehouse(self):
-        response = self.client.post(
-            "/api/warehouse/warehouses/",
-            {
-                "name": "Main Warehouse",
-                "location": "Hyderabad",
-                "manager": self.user.id,
-            },
-            format="json"
-        )
-        assert response.status_code == 201
-        assert Warehouse.objects.count() == 1
+    response = auth_client.get('/api/warehouse/warehouses/?location=Delhi')
 
-    def test_filter_warehouse_by_location(self):
-        Warehouse.objects.create(name='W1',location='Hyderabad',manager=self.user)
-        Warehouse.objects.create(name='W2',location='Delhi',manager=self.user)
+    assert response.status_code == 200
+    assert len(response.data) == 1
 
-        response = self.client.get('/api/warehouse/warehouses/?location=Delhi')
+# ------------------
+# STORAGE TYPE TEST
+# ------------------
+def test_create_storage_type(auth_client):
+    response = auth_client.post(
+        '/api/warehouse/storagetypes/',
+        {
+            'name':'Dry Storage',
+            'temperature_range':'10 to 25c'
+        },
+        format='json'
+    )
 
-        assert response.status_code == 200
-        assert len(response.data) == 1
+    assert response.status_code == 201
+    assert StorageType.objects.count() == 1
 
-    
-    # ------------
-    # STORAGE TYPE TEST
-    # ------------
-    def test_create_storage_type(self):
-        response = self.client.post(
-            "/api/warehouse/storagetypes/",{
-                'name':'Cold Storages',
-                'temperature_range':'-10 to 5C'
-            },
-            format='json'
-        )
-        assert response.status_code == 201
-        assert len(response.data) == 3
+# ----------------
+# ZONE TEST
+# ----------------
+def test_create_zone(auth_client,warehouse,storage_type):
+    response = auth_client.post(
+        '/api/warehouse/zones/',
+        {
+            'name':'Zone A',
+            'warehouse':warehouse.id,
+            'storage_type':storage_type.id
+        },
+        format='json'
+    )
+    assert response.status_code == 201
+    assert Zone.objects.count() == 1
 
-    # ------------
-    # ZONE TESTS
-    # ------------
-    def test_create_zone(self):
-        warehouse = Warehouse.objects.create(
-            name='Main Warehouse',
-            location='Hyderabad',
-            manager=self.user
-        )
+# -----------
+# RACK TEST
+# -----------
+def test_create_rack(auth_client,zone):
+    response = auth_client.post(
+        '/api/warehouse/racks/',
+        {
+            'zone':zone.id,
+            'rack_code':'R001',
+            'max_capacity':100
+        },
+        format='json'
+    )
+    assert response.status_code == 201
+    assert Rack.objects.count() == 1
 
-        storage = StorageType.objects.create(
-            name='Cold Storage',
-            temperature_range='-10 to 25'
-        )
+# -----------
+# BIN TEST
+# -----------
+def test_create_bin(auth_client,rack):
+    response = auth_client.post(
+        '/api/warehouse/bins/',
+        {
+            'bin_code':'BIN001',
+            'max_capacity':100,
+            'current_capacity':50,
+            'rack':rack.id
+        },
+        format='json'
+    )
 
-        response = self.client.post(
-            '/api/warehouse/zones/',
-            {
-                'name':'Zone A',
-                'warehouse':warehouse.id,
-                'storag_type':storage.id
-            },
-            format='json'
-        )
+    assert response.status_code == 201
+    assert Bin.objects.count() == 1
 
-        assert response.status_code == 201
-        assert len(response.data) == 6
+# -----------
+# AUTH TEST
+# ----------
+def test_unauthenticated_access(manager_user):
+    from rest_framework.test import APIClient
 
-    # -----------------
-    # RACK TESTS
-    # ------------------
-    def test_create_rack(self):
-        warehouse = Warehouse.objects.create(
-            name='Main Warehouse',
-            location='Hyderabad',
-            manager=self.user
-        )
-        zone = Zone.objects.create(
-            name='Zone A',
-            warehouse=warehouse
-        )
-        response = self.client.post(
-            '/api/warehouse/racks/',
-            {
-                'zone':zone.id,
-                'rack_code':'R001',
-                'max_capacity':100
-            },
-            format='json'
-        )
+    client = APIClient()
+    response = client.post('/api/warehouse/warehouses/')
+    assert response.status_code == 401
 
-        assert response.status_code == 201
-        assert Rack.objects.count() == 1
-
-
-    # -------------
-    # BIN TESTS
-    # -------------
-    def test_create_bin(self):
-        warehouse = Warehouse.objects.create(
-            name='Main Warehouse',
-            location='Hyderabad',
-            manager=self.user
-        )
-        zone = Zone.objects.create(
-            name='Zone A',
-            warehouse=warehouse
-        )
-        rack = Rack.objects.create(
-            zone = zone,
-            rack_code = 'R001',
-            max_capacity = 100
-        )
-
-        response = self.client.post(
-            '/api/warehouse/bins/',
-            {
-                'bin_code':'B001',
-                'max_capacity':100,
-                'current_capacity':50,
-                'rack':rack.id
-            },
-            format='json'
-        )
-
-        assert response.status_code == 201
-        assert Bin.objects.count() == 1
-        assert Bin.objects.first().is_available is True
-
-
-    # ---------------
-    # AUTH TEST
-    # ---------------
-    def test_unauthenticated_access(self):
-        self.client.force_authenticate(user=None)
-        
-        response = self.client.post('/api/warehouse/warehouses/')
-        assert response.status_code == 401
