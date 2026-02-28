8fb667773ddfd09972fde4db030bfa656b495b9c
Rakesh N <nraki522@gmail.com>
2026-02-28 12:14:53 +0530

    tested using fixtures

--- orders/tests/test_models.py
+++ orders/tests/test_models.py
@@ -1,44 +1,8 @@
-import pytest
-from orders.models import SalesOrder,SalesOrderItem
-from products.models import Product,Category,StorageType
 
-@pytest.mark.django_db
-def test_salesorder_model():
-    salesorder = SalesOrder.objects.create(
-        order_number='AZ001',
-        customer_name='rakesh',
-        status="COMPLETED"
-    )
-    assert salesorder.customer_name == 'rakesh'
+def test_salesorder_model(salesorder):
+    assert salesorder.customer_name == 'Rakesh'
 
-@pytest.mark.django_db
-def test_salesorderitem_model():
-    salesorder = SalesOrder.objects.create(
-        order_number='AB001',
-        customer_name='rakesh',
-        status='COMPLETED'    
-    )
-    category = Category.objects.create(
 
-        name='fruits'
-    )
-    storage = StorageType.objects.create(
-        name='Cold Storage',
-        temperature_range='10 to 25c'
-    )
-    product = Product.objects.create(
-        name='banana',
-        sku='BAN002',
-        storage_type=storage,
-        category=category
-    )
-    salesorderitem = SalesOrderItem.objects.create(
-        order = salesorder,
-        product = product,
-        quantity = 10
-    )
-    
+def test_salesorderitem_model(salesorderitem):
     assert salesorderitem.product.category.name == 'fruits'
-    assert salesorderitem.quantity == 10
-
-    
+    assert salesorderitem.quantity == 2
--- orders/tests/test_views.py
+++ orders/tests/test_views.py
@@ -1,85 +1,48 @@
-import pytest
+
 from orders.models import SalesOrder,SalesOrderItem
-from rest_framework.test import APIClient
-from django.contrib.auth import get_user_model
-from products.models import Category,StorageType,Product
-
-User = get_user_model()
-
-@pytest.mark.django_db
-class TestOrderAPI:
-
-    def setup_method(self):
-        self.client = APIClient()
-        self.user = User.objects.create(
-            email='manager@gmail.com',
-            full_name='Test Manager',
-            password='password123',
-            role='MANAGER'
-        )
-        self.client.force_authenticate(user=self.user)
 
     # -----------------
     # SALESORDER TEST
     # -----------------
 
-    def test_create_salesorder(self):
-        response = self.client.post(
-            '/api/order/salesorders/',
-            {
-                'order_number':'AZ001',
-                'customer_name':'rakesh',
-                'status':'COMPLETED'
-            },
-            format='json'
-        )
+def test_create_salesorder(auth_client):
+    response = auth_client.post(
+        '/api/order/salesorders/',
+        {
+            'order_number':'AZ001',
+            'customer_name':'rakesh',
+            'status':'COMPLETED'
+        },
+        format='json'
+    )
 
-        assert response.status_code == 201
-        assert SalesOrder.objects.count() == 1
+    assert response.status_code == 201
+    assert SalesOrder.objects.count() == 1
 
-    def test_filter_order_number(self):
-        SalesOrder.objects.create(order_number='AB123',customer_name='Rakesh',status='COMPLETED')
-        SalesOrder.objects.create(order_number='AB124',customer_name='Gukesh',status='PENDING')
+def test_filter_order_number(auth_client):
+    SalesOrder.objects.create(order_number='AB123',customer_name='Rakesh',status='COMPLETED')
+    SalesOrder.objects.create(order_number='AB124',customer_name='Gukesh',status='PENDING')
 
-        response = self.client.get('/api/order/salesorders/?order_number=AB123')
+    response = auth_client.get('/api/order/salesorders/?order_number=AB123')
 
-        assert response.status_code == 200
-        assert len(response.data) == 1
+    assert response.status_code == 200
+    assert len(response.data) == 1
 
-    
-    # ---------------------
-    # SALESORDERITEM TEST
-    # ---------------------
-    def test_create_salesorderitem(self):
-        category = Category.objects.create(
-            name='fruits'
-        )
-        storage = StorageType.objects.create(
-            name='Cold Storage',
-            temperature_range='10 to 25c'
-        )
 
-        product = Product.objects.create(
-            name='Banana',
-            sku='BA006',
-            storage_type=storage,
-            category=category
-        )
-        order = SalesOrder.objects.create(
-            order_number='AB002',
-            customer_name='Rakesh',
-            status='COMPLETED'
-        )
-        response = self.client.post(
-            '/api/order/salesorderitems/',
-            {
-                'order':order.id,
-                'product':product.id,
-                'quantity':10
-            },
-            format='json'
-        )
+# ---------------------
+# SALESORDERITEM TEST
+# ---------------------
+def test_create_salesorderitem(auth_client,salesorder,product):
+    response = auth_client.post(
+        '/api/order/salesorderitems/',
+        {
+            'order':salesorder.id,
+            'product':product.id,
+            'quantity':10
+        },
+        format='json'
+    )
 
-        assert response.status_code == 201
-        assert SalesOrderItem.objects.count() == 1
+    assert response.status_code == 201
+    assert SalesOrderItem.objects.count() == 1
 
