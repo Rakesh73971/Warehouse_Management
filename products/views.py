def42081a3c44c0832ed2f1510118691ab5b358b
Rakesh N <nraki522@gmail.com>
2026-03-06 23:05:07 +0530

    used caching

--- products/views.py
+++ products/views.py
@@ -3,19 +3,34 @@ from .models import Product,Category,Inventory,StockMovement
 from .serializers import ProductSerializer,CategorySerializer,InventorySerializer,StockMovementSerializer
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
+from django.core.cache import cache
+from django.utils.decorators import method_decorator
 
+
+class CacheListMixin:
+    @method_decorator(cache_page(60*5))
+    @method_decorator(vary_on_headers('Authorization'))
+    def list(self,request,*args,**kwargs):
+        return super().list(request,*args,**kwargs)
+    
+
+    @method_decorator(cache_page(60*5))
+    @method_decorator(vary_on_headers('Authorization'))
+    def retrieve(self,request,*args,**kwargs):
+        return super().retrieve(request,*args,**kwargs)
 # Create your views here.
-
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(CacheListMixin,viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = {
-    'name': ['exact', 'icontains']
+        'name': ['exact', 'icontains'],
     }
 
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductViewSet(CacheListMixin,viewsets.ModelViewSet):
     queryset = Product.objects.select_related('category','storage_type').all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
@@ -25,15 +40,26 @@ class ProductViewSet(viewsets.ModelViewSet):
         'category': ['exact'],
         'storage_type': ['exact'],
     }
+    def perform_create(self, serializer):
+        serializer.save()
+        cache.clear()
 
-class InventoryViewSet(viewsets.ModelViewSet):
+    def perform_update(self, serializer):
+        serializer.save()
+        cache.clear()
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        cache.clear()
+
+class InventoryViewSet(CacheListMixin,viewsets.ModelViewSet):
     queryset = Inventory.objects.select_related('product','bin').all()
     serializer_class = InventorySerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['product','bin']
 
-class StockMovementViewSet(viewsets.ModelViewSet):
+class StockMovementViewSet(CacheListMixin,viewsets.ModelViewSet):
     queryset = StockMovement.objects.select_related('product','bin').all()
     serializer_class = StockMovementSerializer
     permission_classes = [IsAuthenticated]
