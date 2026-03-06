93315258d397e504c344926206a0ad0e2cb3de4a
Rakesh N <nraki522@gmail.com>
2026-03-06 22:46:24 +0530

    added caching to warehouse app

--- warehouse/views.py
+++ warehouse/views.py
@@ -5,10 +5,20 @@ from django.db.models import Prefetch
 from .models import Warehouse,Zone,Rack,Bin,StorageType
 from .serializers import WarehouseSerializer,ZoneSerializer,RackSerializer,BinSerializer,StorageTypeSerializer
 from accounts.permissions import IsAdmin,IsManager
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from django.views.decorators.vary import vary_on_headers
+from django.core.cache import cache
 
+class CacheListMixin:
+
+    @method_decorator(cache_page(60 * 5))
+    @method_decorator(vary_on_headers("Authorization"))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
 #Warehouse Viewset
-class WarehouseViewSet(viewsets.ModelViewSet):
+class WarehouseViewSet(CacheListMixin,viewsets.ModelViewSet):
     queryset = Warehouse.objects.select_related('manager').all()
     serializer_class = WarehouseSerializer
     permission_classes = [IsAuthenticated]
@@ -16,32 +26,40 @@ class WarehouseViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['is_active','location']
 
+    
+    
+
 
 #Zone ViewSet
-class ZoneViewSet(viewsets.ModelViewSet):
+class ZoneViewSet(CacheListMixin,viewsets.ModelViewSet):
     queryset = Zone.objects.select_related('warehouse','storage_type').all()
     serializer_class = ZoneSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['warehouse','storage_type']
 
+    
+
 # Rack ViewSet
-class RackViewSet(viewsets.ModelViewSet):
+class RackViewSet(CacheListMixin,viewsets.ModelViewSet):
     queryset = Rack.objects.select_related('zone').all()
     serializer_class = RackSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['zone']
 
+
 # Bin Viewset
-class BinViewSet(viewsets.ModelViewSet):
+class BinViewSet(CacheListMixin,viewsets.ModelViewSet):
     queryset = Bin.objects.select_related('rack').all()
     serializer_class = BinSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['rack','is_available']
 
-class StorageTypeViewSet(viewsets.ModelViewSet):
+
+
+class StorageTypeViewSet(CacheListMixin,viewsets.ModelViewSet):
     queryset = StorageType.objects.all()
     serializer_class = StorageTypeSerializer
     permission_classes = [IsAuthenticated]
